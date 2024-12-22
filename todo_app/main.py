361d5ba3cc55504695798be6fbc4776b1839e728
Marjy G <marjguery@gmail.com>
2024-12-22 00:16:36 -0500

    Edit time label

--- todo_app/main.py
+++ todo_app/main.py
@@ -2,7 +2,7 @@ from functions import get_todos, write_todos
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
-print("It is", now)
+print("The time is now", now)
 
 while True: 
     user_action = input("type add, show, edit, complete, or exit: ")
