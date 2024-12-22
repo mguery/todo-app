09460d6bd9e94360467dbb5ee13c1b2f78620889
Marjy G <marjguery@gmail.com>
2024-12-22 01:00:21 -0500

    Create basic GUI, no interactions

--- todo_app/functions.py
+++ todo_app/functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath=r'C:\Users\msgue\OneDrive\Desktop\Marjy\Projects\Python\Python Mega Course\todo_app\files\todos.txt'):
+FILEPATH = r"C:\Users\msgue\OneDrive\Desktop\Marjy\Projects\Python\Python Mega Course\todo_app\files\todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """
     Read text file and return list of to-dos
     """
@@ -7,7 +9,7 @@ def get_todos(filepath=r'C:\Users\msgue\OneDrive\Desktop\Marjy\Projects\Python\P
     return todos_local
 
 
-def write_todos(todos_arg, filepath=r'C:\Users\msgue\OneDrive\Desktop\Marjy\Projects\Python\Python Mega Course\todo_app\files\todos.txt'):
+def write_todos(todos_arg, filepath=FILEPATH):
     """
     Write to-dos in text file
     """
